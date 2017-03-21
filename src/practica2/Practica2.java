0eb9f836c9c719aa44d6c2b05484da032e79258e
Agustin Bettati <bettatiagustin@gmail.com>
2017-03-21 18:55:09 -0300

    Added javadoc

--- src/practica2/Practica2.java
+++ src/practica2/Practica2.java
@@ -9,37 +9,68 @@ package practica2;
  */
 public class Practica2 implements Practice2{
 
-    public static void main(String[] args) {
-
-    }
-
+    /**
+     * Calculate te summation of i from m to n.
+     * m must be smaller or equal to n.
+     * @param m start of the summation
+     * @param n end of the summation
+     * @return
+     */
     @Override
     public long exercise8(int m, int n) {
-        return 0;
+        if(m > n){
+            throw new IllegalArgumentException();
+        }
+        int result = 0;
+
+        for(int i = m; i<= n; i++){
+            result += i;
+        }
+        return result;
     }
 
+    /**
+     * Calculate the double summation of j from m to n, and k from r to s
+     * of i * k .
+     *
+     *
+     * @param m start of the summation of j
+     * @param n end of the summation of j
+     * @param r start of the summation of k
+     * @param s end of the summation of k
+     * @return
+     */
     @Override
     public long exercise9(int m, int n, int r, int s) {
         long result = 0;
         for (int j = m; j <= n; j++) {
-            for (int k = r; j <= s; k++) {
-                result = result + (j * k);
+            for (int k = r; k <= s; k++) {
+                result += (j * k);
             }
         }
         return result;
     }
 
+    /**
+     * Calculates the summation of i * 2^i from 1 to n
+     * @param n end of the summation
+     * @return
+     */
     @Override
     public long exercise10(int n) {
-        int index=n;
         long result=0;
-        while(n>0){
-            result=result+n* (long)Math.pow(2,n);
-            index--;
+        for(int i = 1; i <= n; i++){
+            result += n* (long)Math.pow(2,n);
         }
         return result;
     }
 
+    /**
+     * Calculate the summation of i * x^i from 0 to n given a value of x
+     * @param n
+     * @param x
+     * @return
+     */
     @Override
     public double exercise11Sumation(int n, int x) {
         double result = 0;
@@ -51,6 +82,13 @@ public class Practica2 implements Practice2{
         return result;
     }
 
+    /**
+     * Resolves the series of i * x^i from 0 to n given a value of x using
+     * a specific formula
+     * @param n
+     * @param x
+     * @return
+     */
     @Override
     public double exercise11Formula(int n, int x) {
         return ((n*Math.pow(x, n+2) - (n+1)*Math.pow(x, n+1) + x)/Math.pow(x-1,2));
