5b4ed64a6632da63f254bb30257b195a537fcb0a
Marcos khabie <marcos.khabie@ing.austral.edu.ar>
2017-05-23 11:45:21 -0300

    gauss

--- src/practica7/Solution.java
+++ src/practica7/Solution.java
@@ -18,11 +18,47 @@ public class Solution implements TP4 {
 
     @Override
     public double[] exercise5WithoutPivoteo(double[][] coefficients, double[] independentTerms) {
+        for (int i = 0; i <coefficients.length ; i++) {
+
+            double valueToDivide= coefficients[i][i];
+            if (i!=coefficients.length-1) {
+                double valueToMultiply = coefficients[i+1][i];
+                independentTerms[i] = independentTerms[i] / valueToDivide;
+                independentTerms[i + 1] = independentTerms[i + 1] - independentTerms[i] * valueToMultiply;
+                for (int j = 0; j < coefficients.length; j++) {
+
+
+                    coefficients[i][j] = coefficients[i][j] / valueToDivide;
+                    coefficients[i + 1][j] = coefficients[i + 1][j] - coefficients[i][j] * valueToMultiply;
+                }
+            }
+            else {
+                independentTerms[i]= independentTerms[i]/valueToDivide;
+                coefficients[i][i]= coefficients[i][i]/valueToDivide;
+            }
+
+            
+        }
         return new double[0];
     }
 
     @Override
     public double[] exercise5PartialPivoteo(double[][] coefficients, double[] independentTerms) {
+
+        for (int i = 0; i <coefficients.length ; i++) {
+            double maxValue=coefficients[i][i];
+            for (int j = 0; j <coefficients.length ; j++) {
+                if (maxValue<coefficients[i][j]){
+                    maxValue=coefficients[i][j];
+                }
+
+            }
+            for (int k = 0; k <coefficients.length ; k++) {
+
+            }
+
+
+        }
         return new double[0];
     }
 
